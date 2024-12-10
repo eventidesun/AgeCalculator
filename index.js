869b81dfe68494cdfb48233e2b5429b472258fea
Vaishnavi Gudimella <118631877+eventidesun@users.noreply.github.com>
2024-12-10 18:08:57 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
-const btnEl = document.getElementById("calculate");
-const billInput = document.getElementById("bill");
-const tipInput = document.getElementById("tip");
-const totalSpan = document.getElementById("total");
- 
+// we have all the elements using the method called getElementById
+const btnEl = document.getElementById("btn");
+const birthdayEl = document.getElementById("birthday");
+const resultEl = document.getElementById("result");
 
-function calculateTotal() {
-    const billValue = billInput.value;
-    const tipValue = tipInput.value;
-    const totalValue = billValue * (1 + tipValue / 100);
+// this function calculates the age
+function calculateAge() {
 
-    // console.log(totalValue);
-    totalSpan.innerText = totalValue.toFixed(2);
+    const birthdayValue = birthdayEl.value;
+
+    // console.log(birthdayValue);
+    if (birthdayValue === "") { // if nothing is entered, we get an alert saying that we need to enter the birthday date
+        alert("Please enter your birthday")
+    } else { // if the birthday date is entered, we then call the getAge function
+        const age = getAge(birthdayValue);
+        resultEl.innerText = `Your are ${age} ${age > 1 ? "years" : "year"} old`; // we follow this format - if the age is 1 then we use "year" otherwise we use "years"
+    }
 }
 
-btnEl.addEventListener("click", calculateTotal)
+function getAge(birthdayValue) { // we pass the birthdayValue here as a paremeter
+    const currentDate = new Date()
+    const birthdayDate = new Date(birthdayValue); // we need to calculate the age based on the current age and the birthday date
+    let age = currentDate.getFullYear() - birthdayDate.getFullYear(); // here, we calculate the age
+    const month = currentDate.getMonth() - birthdayDate.getMonth();
+    // console.log(birthdayDate.getFullYear());
+
+    // if the month is less than 0 (that is, the birthday month) or when the month is 0 but the day is less than the birthday day then the age is decreased by 1
+    if (month < 0 || (month === 0 && currentDate.getDate() < birthdayDate.getDate())) {
+        age--;
+    }
+    return age; // then we return the age
+}
+
+// we have created an event listener such that when the button (btnEl) is clicked, teh function calculateAge is called
+btnEl.addEventListener("click", calculateAge) 
